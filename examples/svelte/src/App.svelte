efb9ed0584539e9c995be920d8153ce012aa935b
Spice-Z <jigenbakdaaan@gmail.com>
2020-10-27 20:18:50 +0900

    feat:style

--- examples/svelte/src/App.svelte
+++ examples/svelte/src/App.svelte
@@ -1,32 +1,36 @@
 <script>
-	import Footer from 'package-practice';
-	export let name;
+  import Footer from "svelte-to-x";
+  export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<Footer />
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <Footer />
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
--- src/Footer.svelte
+++ src/Footer.svelte
@@ -2,4 +2,12 @@
   export let name = "world";
 </script>
 
-<h1>Hello {name} from Svelte component!</h1>
+<style>
+  .title {
+    color: purple;
+    font-family: "Comic Sans MS", cursive;
+    font-size: 2em;
+  }
+</style>
+
+<h1 class="title">Hello {name} from Svelte component!</h1>
