66de41f6fcb273e579363b24bc48cc492173a08c
Spice-Z <jigenbakdaaan@gmail.com>
2020-10-27 09:27:08 +0900

    init

---
+++ examples/vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div ref="target" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Footer from "package-practice";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  mounted() {
+    new Footer({
+      target: this.$refs.target,
+      props: {
+        name: "world",
+      },
+    });
+  },
+};
+</script>
